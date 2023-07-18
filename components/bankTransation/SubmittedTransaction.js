362095478c18359f47a13767e2d902528263c277
mohamedashik-salludeen <69164259+mohamedashik-salludeen@users.noreply.github.com>
2023-07-18 15:55:44 +0530

    Update SubmittedTransaction.js

--- components/bankTransation/SubmittedTransaction.js
+++ components/bankTransation/SubmittedTransaction.js
@@ -1,60 +1,107 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import Grid from "@mui/material/Grid";
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
 function SubmittedTransaction() {
     const [loginUser, setLoginUser] = useState({})
     const [totaluserDetails, settotaluserDetails] = useState([])
-   
+    const usenavigate = useNavigate();
+
+    const NavMainpage = (e) => {
+        e.preventDefault();
+        usenavigate('/mainpage')
+    }
+
 
     useEffect(() => {
         let userData = JSON.parse(sessionStorage.getItem("userdetails"));
+        let loginUserData = []
         setLoginUser(userData)
-
-        axios.get("http://localhost:3001/transactions")
+        axios.get("http://localhost:3030/transactions")
             .then((res) => {
-                settotaluserDetails(res.data)
+                if (userData.name !== "admin123") {
+                    res.data.forEach((data) => {
+                        if (data[userData.name]) {
+                            data[userData.name].forEach((itm) => {
+                                loginUserData.push(itm)
+                            })
+                        }
+                    })
+                    settotaluserDetails(loginUserData)
+                }
+                else {
+                    res.data.forEach((data) => {
+                        Object.values(data).forEach((itm) => {
+                            if (itm.length) {
+                                itm.forEach((datas) => {
+                                    loginUserData.push(datas)
+                                })
+                            }
+                        })
+                    })
 
+                    settotaluserDetails(loginUserData)
+                }
             })
             .catch((err) => err.message)
-
-
-
     }, [])
-    return (
-        <Grid container  >
-            <Typography style={{marginTop:"20PX",paddingLeft:"20PX"}}>Welcome : {loginUser.role ==="adminUser"?  <b>{loginUser.name}</b>:loginUser.name}</Typography>
-            <Grid item xs={12} sx={{ mt: 3 }} >
-                <Table>
-                    <TableHead style={{ backgroundColor: '#2979ff', color: 'white' }}>
-                        <TableRow>
 
-                            <TableCell sx={{ fontWeight: "700" }}>Sno</TableCell>
-                            <TableCell sx={{ fontWeight: "700" }}>Total Data</TableCell>
+
+
+    return (
+        <Grid container style={{ position: "relative", top: "92px", padding: "20px" }}>
+            <div className='displayTransc'>Total Transactions: <b>{totaluserDetails.length}</b></div>
+            
+            <Grid item xs={12} sx={{ mt: 2, }} >
+            <div style={{fontSize:"20px"}}>{loginUser.name === "admin123" ? "Admin will see all the transactions of the Users" :null}</div>
+            <div style={{fontSize:"22px",fontWeight:"bold",marginBottom:"10px",width:"100%",textAlign:"center"}}>Transactions History</div>
+                <Table sx={{boxShadow: 3}}>
+                    <TableHead style={{ backgroundColor: '#2979ff',marginTop:"10px" }}>
+                        <TableRow>
+                            <TableCell sx={{ fontWeight: "700", color: "#fff" }}>S.No</TableCell>
+                            <TableCell sx={{ fontWeight: "700", color: "#fff" }}>Reference Num</TableCell>
+                            <TableCell sx={{ fontWeight: "700", color: "#fff" }}> Customer Name</TableCell>
+                            <TableCell sx={{ fontWeight: "700", color: "#fff" }}>Transfer Amount</TableCell>
+                            <TableCell sx={{ fontWeight: "700", color: "#fff" }}> Transfer Currency</TableCell>
+                            <TableCell sx={{ fontWeight: "700", color: "#fff" }}> Beneficiary Bank</TableCell>
+
                         </TableRow>
                     </TableHead>
-                 
-                   <TableBody>
+
+                    <TableBody>
                         {totaluserDetails.map((data, ind) => {
                             return (
-                                <TableRow key={ind}>
-                                    <TableCell>{ind}</TableCell>
-                                    <TableCell>{JSON.stringify(data)}</TableCell>
+                                <TableRow key={ind} >
+                                    <TableCell  sx={{ fontWeight: "900"}}>{ind + 1}</TableCell>
+                                    <TableCell>{data.referenceNum}</TableCell>
+                                    <TableCell >{data.customerName}</TableCell>
+                                    <TableCell>{data.transferAmount}</TableCell>
+                                    <TableCell> {data.transferCurrency}</TableCell>
+                                    <TableCell>{data.beneficiaryBank}</TableCell>
                                 </TableRow>
                             )
                         })}
                     </TableBody>
-                   
+
 
                 </Table>
             </Grid>
-        </Grid>
+
+            <Grid item xs={3} >
+                <Button type="submit" variant="contained" sx={{ mt: 1 }} onClick={(e) => NavMainpage(e)}>Go To MainPage </Button>
+            </Grid>
+
+
+
+
+        </Grid >
 
     );
 }
